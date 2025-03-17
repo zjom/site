7918e0afa779d604d27e2de3b98834a7882fb0a3
Zihan <admin@zihanjin.com>
2025-03-17 11:44:12 +1100

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
---
+++ src/content/blog/hi.md
@@ -0,0 +1,7 @@
+---
+title: "First post"
+description: "Lorem ipsum dolor sit amet"
+pubDate: "Jul 08 2022"
+---
+
+# Hi
